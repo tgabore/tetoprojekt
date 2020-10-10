7956725e4ff6e42804297985ec746d710c25461e
qwemeli <qwemeli@gmail.com>
2020-10-10 23:09:41 +0200

    Create tetocserepdb.py

---
+++ tetocserepdb.py
@@ -0,0 +1,27 @@
+print( "készítették: tgabore,lucc0413,qwemeli" )
+print( "            "           , "2020-10-13" )
+print(
+
+
+)
+a = 0.25
+b = 0.12
+cs = ( a * b )
+print("cserépméret: ", cs , "m2" )
+
+
+h = 23
+sz = 9.6
+m = 3.2
+sz2= 4.8
+import math 
+oldal = math.sqrt(m**2 + sz2**2)
+
+t = 2*(h * oldal)
+
+print("tető területe: ", t , "m2" )  
+
+darab = t/cs
+
+print('%d darab cserépre lesz szükség.'%darab)
+
